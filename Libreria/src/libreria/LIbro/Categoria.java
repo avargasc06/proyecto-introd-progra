3010c66f089820d927e8214e4f9fa786642c7fcc
dilshotV <dilshotvargas@gmail.com>
2026-03-23 17:30:31 -0600

    Add files via upload

---
+++ Libreria/src/libreria/LIbro/Categoria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package libreria.LIbro;
+
+/**
+ *
+ * @author dilsh
+ */
+public enum Categoria {
+    Literatura,Historia,Filosofía,Arte,Infantil,Accion
+
+
+
+}
---
+++ Libreria/src/libreria/LIbro/Libro.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package libreria.LIbro;
+
+/**
+ *
+ * @author dilsh
+/yo lo hice asi ya que el del libro,tambien tienen un enum/ */  
+public class Libro {
+    public String Nombre;
+    public String Autor;
+    public String Codigo;
+    public String FechaPublicacion;
+    public int cantiadad;
+    
+    
+    
+    
+    
+}
